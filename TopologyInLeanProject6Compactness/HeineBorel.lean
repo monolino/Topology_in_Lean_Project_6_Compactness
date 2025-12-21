ad340580beeee592ad75274f9e035b594e7a6453
Julia Würsch <jul.wuersch@gmail.com>
2025-12-21 18:40:12 +0100

    added R^n with euclideandist,topology etc.

---
+++ TopologyInLeanProject6Compactness/HeineBorel.lean
@@ -0,0 +1,83 @@
+import TopologyInLeanProject6Compactness.Definitions.Compactness
+import TopologyInLeanProject6Compactness.Definitions.MetricSpaces
+
+
+def Rn (n : ℕ) : Type := (Fin n → ℝ)
+
+--Euclidean distance
+@[simp]
+noncomputable def Eucldist (n : ℕ) (x y : Rn n) : ℝ := Real.sqrt (∑ i , (x i- y i)^2)
+
+noncomputable instance Rn_metric_space (n : ℕ) : MetricSpace (Rn n) := {
+  dist := Eucldist n
+  dist_self := by
+    intro x
+    simp
+  dist_comm := by
+    intro x y
+    simp
+    congr
+    ext i
+    ring
+  dist_triangle := by
+    intro x y z
+    simp
+    sorry
+  eq_of_dist_eq_zero := by
+    intro x y hxy
+    simp at hxy
+    sorry
+}
+
+noncomputable def Rn_topology (n : ℕ) : Topology (Rn n) :=
+{ Open := fun U ↦ ∀ x ∈ U, ∃ ε > 0, ∀ y, Eucldist n x y < ε → y ∈ U,
+  Open_univ := by
+    intro x hx
+    use 1
+    constructor
+    · linarith
+    intro y hy
+    trivial
+  Open_inter := by
+    intros U V hU hV x hx
+    rcases hx with ⟨hxU, hxV⟩
+    rcases hU x hxU with ⟨ Uε ,hUε, hUε_prop⟩
+    rcases hV x hxV with ⟨ Vε, hVε , hVε_prop⟩
+    let total_ε := min Uε Vε
+    use total_ε
+    constructor
+    · exact lt_min hUε hVε
+    intro y hy
+    have hU' : Eucldist n x y < Uε :=
+      lt_of_lt_of_le hy (min_le_left _ _)
+    have hV' : Eucldist n x y < Vε :=
+      lt_of_lt_of_le hy (min_le_right _ _)
+    exact ⟨hUε_prop y hU', hVε_prop y hV'⟩
+  Open_sUnion := by
+    intros S hS x hx
+    rcases hx with ⟨U, hUinS, hxU⟩
+    rcases hS U hUinS x hxU with ⟨ε, hε_pos, hUprop⟩
+    use ε
+    constructor
+    · exact hε_pos
+    intro y hy
+    exact ⟨U, hUinS, hUprop y hy⟩
+}
+
+
+
+
+def Bounded (K : Set (Rn n)) : Prop := ∃ (r : ℝ) (hr : r > 0) (x₀ : Rn n), ∀ x ∈ K, dist x x₀ < r
+
+
+
+theorem HeineBorel {n : ℕ} (K : Set (Rn n)) : Compact K ↔ Closed K ∧ Bounded K := by
+  constructor
+  case mp =>
+    intro comp
+    constructor
+    case right =>
+      rw[Bounded]
+      let U : K → Set (Rn n) := λ x ↦ Metric.ball x 1 --open Cover of balls with radius 1 around each point in K
+      have U_openCover : @openCover (Rn n) (Rn_topology n) K := by
+        sorry
