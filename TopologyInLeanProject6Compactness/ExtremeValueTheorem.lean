f008fcd2ae7a2fb1644dddce8ae94e9d4b4d460f
Julia Würsch <jul.wuersch@gmail.com>
2026-01-03 13:51:14 +0100

    added by

--- TopologyInLeanProject6Compactness/ExtremeValueTheorem.lean
+++ TopologyInLeanProject6Compactness/ExtremeValueTheorem.lean
@@ -12,7 +12,7 @@ variable (n : ℕ)
 theorem ExtremeValueTheorem (K : Set (Rn n)) (hK : Compact K) (f : (Rn n) → (Rn 1))
   (f_cont : ContOn f K) :
   ∃ x_min x_max : K,  ∀ x : K,
-  f x_min ≤ f x ∧ f x ≤ f x_max :=
+  f x_min ≤ f x ∧ f x ≤ f x_max := by
   have hfKCompact : Compact (f '' K) := Compact.image_on hK f_cont
   have h_C_B : Closed (f '' K) ∧ Bounded 1 (f '' K) :=
     (HeineBorel (n := 1) (K := f '' K)).mp hfKCompact
